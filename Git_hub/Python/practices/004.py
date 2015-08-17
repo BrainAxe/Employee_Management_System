1adcbc53c64096526f05d9b4ae58a1b8f1fcef0d
BrainAxe <tanzimrizwan@gmail.com>
2015-08-17 20:01:25 +0600

    Duplicates

--- Git_hub/Python/practices/004.py
+++
@@ -1,7 +0,0 @@
-l = ['a', 'b', 'f', 'f', 'b', 'b']
-a = len(l)
-print a
-c = {}
-for i in l:
-    b = l.pop(0)
-    print b
--- Git_hub/Python/practices/join.py
+++
@@ -1,9 +0,0 @@
-ten_thing = "Apples Oranges Crows Telephone Light Sugar"
-
-stuff = ten_thing.split(' ')
-
-print ' '.join(stuff)
-
-#a = join(' ', stuff)
-
-#print a
